ba3368f92e3e10b861b372bb7e011cdeea6a68a9
Yulan Huang <yulanh7@gmail.com>
2025-08-21 14:14:01 +1000

    Version: 2.1.0

--- attendance-plugin.php
+++ attendance-plugin.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Attendance Plugin
  * Description: Manage attendance (split structure).
- * Version: 2.0.2
+ * Version: 2.1.0
  * Author: Rachel Huang
  * Text Domain: attendance-plugin
  */
